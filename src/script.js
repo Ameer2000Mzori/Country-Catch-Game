a645f11da83a9bbb89316ac883e0897b3f82376f
Ameer <ameerapex496@gmail.com>
2023-12-14 15:54:06 +0100

    added start logic + added styling

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 // selecting elements
+var startBtn = document.getElementsByClassName("start-Btn")[0];
 var startWrap = document.getElementsByClassName("start-Wrap")[0];
 var choiceWrap = document.getElementsByClassName("choice-Wrap")[0];
 var CardEl = document.getElementsByClassName("Card")[0];
@@ -9,4 +10,10 @@ var playScore = document.getElementsByClassName("play-Score")[0];
 var playField = document.getElementsByClassName("play-Field")[0];
 // gelobal varibales
 // functions
+var startGame = function () {
+    // transform: translateY(-200%);
+    startWrap.style.transform = "translateY(-100%)";
+    choiceWrap.style.transform = "translateY(-100%)";
+};
 // event lisnters
+startBtn.addEventListener("click", startGame);
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,10 @@
 // selecting elements
-const startWrap = document.getElementsByClassName("start-Wrap")[0];
-const choiceWrap = document.getElementsByClassName("choice-Wrap")[0];
+const startBtn = document.getElementsByClassName("start-Btn")[0];
+const startWrap: any = document.getElementsByClassName("start-Wrap")[0];
+const choiceWrap: any = document.getElementsByClassName("choice-Wrap")[0];
 const CardEl = document.getElementsByClassName("Card")[0];
 const choiceImg = document.querySelectorAll("#choice-Img");
-const playWrap = document.getElementsByClassName("play-Wrap")[0];
+const playWrap: any = document.getElementsByClassName("play-Wrap")[0];
 const playTime = document.getElementsByClassName("play-Time")[0];
 const playScore = document.getElementsByClassName("play-Score")[0];
 const playField = document.getElementsByClassName("play-Field")[0];
@@ -11,5 +12,11 @@ const playField = document.getElementsByClassName("play-Field")[0];
 // gelobal varibales
 
 // functions
+const startGame = () => {
+  // transform: translateY(-200%);
+  startWrap.style.transform = `translateY(-100%)`;
+  choiceWrap.style.transform = `translateY(-100%)`;
+};
 
 // event lisnters
+startBtn.addEventListener("click", startGame);
