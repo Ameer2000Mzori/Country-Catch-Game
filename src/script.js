1c8f5e6c3d41b8b1ba613f44cda8035ece4889b7
Ameer <ameerapex496@gmail.com>
2023-12-14 15:46:06 +0100

    added comments, selected elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-console.log("hallo world");
+// selecting elements
+var startWrap = document.getElementsByClassName("start-Wrap")[0];
+var choiceWrap = document.getElementsByClassName("choice-Wrap")[0];
+var CardEl = document.getElementsByClassName("Card")[0];
+var choiceImg = document.querySelectorAll("#choice-Img");
+var playWrap = document.getElementsByClassName("play-Wrap")[0];
+var playTime = document.getElementsByClassName("play-Time")[0];
+var playScore = document.getElementsByClassName("play-Score")[0];
+var playField = document.getElementsByClassName("play-Field")[0];
+// gelobal varibales
+// functions
+// event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log("hallo world");
+// selecting elements
+const startWrap = document.getElementsByClassName("start-Wrap")[0];
+const choiceWrap = document.getElementsByClassName("choice-Wrap")[0];
+const CardEl = document.getElementsByClassName("Card")[0];
+const choiceImg = document.querySelectorAll("#choice-Img");
+const playWrap = document.getElementsByClassName("play-Wrap")[0];
+const playTime = document.getElementsByClassName("play-Time")[0];
+const playScore = document.getElementsByClassName("play-Score")[0];
+const playField = document.getElementsByClassName("play-Field")[0];
+
+// gelobal varibales
+
+// functions
+
+// event lisnters
